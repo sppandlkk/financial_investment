c0f4ee72bc16f2dedc929a36eebc381b4e8914a3
sppandlkk <u900222@gmail.com>
2020-03-05 16:19:24 -0800

    Update first.R

--- code/first.R
+++ code/first.R
@@ -1,53 +1,27 @@
 rm(list=ls())
 source("./code/Utils.R")
 library(lubridate)
-stocks <- c("BND", "VOO", "VTI", "VWO") 
+stocks <- c("BND", "VOO", "VTI", "VWO", "BLV") 
 dataUse <- GrabStockData(stocks)
 
 
-
-
-
-ans1 <- sapply(stocks, function(s) {
-  FixedAmountPeriodically(dataUse = dataUse,
-                                 stock = s,
-                                 start_date = "2005-01-01",
-                                 end_date = "2020-01-01",
-                                 candance = "month",
-                                 fixed_amount = 1000)[[2]]
+### comparing fixed amount and fixed value
+do.call(rbind,
+  lapply(stocks, function(s) {
+    
+    subsetData <- SubsetDataByCandance(dataUse[[s]], "2010-01-01", "2020-12-31", "month")
+    
+    a1 <- FixedAmountPeriodically(subsetData = subsetData,
+                                   fixed_amount = 1000)[[2]]
+    a1$method <- "fixed_amount"
   
-  
-})
+    a2 <- FixedValuePeriodically(subsetData = subsetData,
+                            fixed_value = 1000)[[2]]
+    a2$method <- "fixed_value"
+    
+    data.table(stock = s, rbind(a1, a2))
+    
+  })
+)
    
-
-
-
-
-ans2 <- sapply(stocks, function(s) {
-  FixedValuePeriodically(dataUse = dataUse,
-                          stock = s,
-                          start_date = "2005-01-01",
-                          end_date = "2020-01-01",
-                          candance = "month",
-                          fixed_value = 1000)[[2]]
-  
-  
-})
-
-ans1
-ans2
-
-
-
-
-dd <-FixedValuePeriodically(dataUse = dataUse,
-                       stock = "VOO",
-                       start_date = "2005-01-01",
-                       end_date = "2020-01-01",
-                       candance = "month",
-                       fixed_value = 1000)[[1]]
-
-### how about we reverse engineering
-stock <- dataUse[["VOO"]]
-bond <- 
 
