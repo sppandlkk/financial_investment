651559151c93f74b09587100ec2a4d8be8cd44b3
sppandlkk <u900222@gmail.com>
2020-03-07 20:50:47 -0800

    new function

--- code/first.R
+++ code/first.R
@@ -6,30 +6,23 @@ dataUse <- GrabStockData(stocks)
 
 
 ### comparing fixed amount and fixed value
-ans <- do.call(rbind,
-  lapply(stocks, function(s) {
-    
-    subsetData <- SubsetDataByCandance(dataUse[[s]], "2011-01-01", "2019-12-31", "month")
-    
-    a1 <- FixedAmountPeriodically(subsetData = subsetData,
-                                   fixed_amount = 1000)
-    # a1$result$sd_purchase_value <- sd(a1$data$purchase_value)
-    a1$result$method <- "fixed_amount"
-    
-  
-    a2 <- FixedValuePeriodically(subsetData = subsetData,
-                            fixed_value = 1000)
-    # a2$result$sd_purchase_value <- sd(a2$data$purchase_value)
-    a2$result$method <- "fixed_value"
-    
-    data.table(stock = s, rbind(a1$result, a2$result))
-    
-  })
-)
-ans
 
+result <- CompareFixedAmountWithFixedValue(stocks,
+                                           "2011-01-01",
+                                           "2019-12-31",
+                                           "month",
+                                           c(1, 1.5, 2, 999))
+result
+
+###
 VOO <- SubsetDataByCandance(dataUse[["VOO"]], "2011-01-01", "2019-12-31", "month")
 rr <- FixedValuePeriodically(subsetData = VOO,
-                       fixed_value = 1000)$data
+                       fixed_value = 1000,
+                       max_capacity = 2)$data
+FixedValuePeriodically(subsetData = VOO,
+                             fixed_value = 1000,
+                             max_capacity = 2)$result
+
+
 hist(rr$purchase_value, breaks = 50, main = "")
 summary(rr$purchase_value)
